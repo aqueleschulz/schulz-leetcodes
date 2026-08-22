ec6141b79e70ab34610b8cb2cbb7fede36ec5b72
João Schulz <65256627+aqueleschulz@users.noreply.github.com>
2026-08-22 17:10:11 -0300

    Time: 42 ms (57.91%), Space: 53.2 MB (73.49%) - LeetHub

---
+++ 2809-create-hello-world-function/2809-create-hello-world-function.js
@@ -0,0 +1,15 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        const text = 'Hello World'
+        return text;
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
